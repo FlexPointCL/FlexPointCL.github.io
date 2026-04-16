dee62e33761f6b9ea1d67053a810af5d43041f47
Jorge Corvalan F <jorgecorvalanf@MacBook-Air-de-Jorge.local>
2026-04-16 14:38:11 -0400

    fix: logo hero grande, sin btn ROI; roi/ tipografia, logo, flexpointcito mejorado, PDF ejecutivo

---
+++ roi/_next/static/chunks/0ozevf33u7ram.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,34162,t=>{t.v(s=>Promise.all(["static/chunks/03fkavwq3ik5i.js","static/chunks/15bou659b91f0.js"].map(s=>t.l(s))).then(()=>s(55749)))}]);
